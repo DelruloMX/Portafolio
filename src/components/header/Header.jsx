f5fc0fa6e964b1ca4cfda18286ce9c9aebf8c1c7
Raul <Raul@DESKTOP-SG00DPM>
2023-04-24 09:45:26 -0600

    rm

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Typical from 'react-typical'
+
 import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocials'
 
@@ -11,22 +11,7 @@ const Header = () => {
       <div className="container header__container">
       {" "}
         <h5>
-          {" "}
-          <Typical
-             loop={Infinity}
-             steps={[
-              "  Hello I'm",
-                 1000,
-                 " welcome to ",
-                 5000,
-                 "Download my CV",
-                 8000,
-                 "Contact us now!",
-                 10000,
-                 "swipe for more information",
-                 11000,
-             ]}
-          />
+        132
         
         </h5>
         {" "}
